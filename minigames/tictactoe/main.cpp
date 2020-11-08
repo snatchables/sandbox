4ab4282dbca72ad5d9124676820f9702e282b0f2
snatchables <joshiebray@hotmail.com>
2020-11-08 19:55:20 +1000

    added some user input bullshit, but cout is pretty shitty so cant do much for now.

--- minigames/tictactoe/main.cpp
+++ minigames/tictactoe/main.cpp
@@ -1,21 +1,52 @@
 #include <iostream>
 
-int main()
+// create a 2d array and init all values to 0
+int positions[4][7] = {};
+const int rows = 4;
+const int columns = 7;
+
+void printPositions()
 {
-	// create a 2d array and init all values to 0
-	int positions[3][3] = {};
-	const int rows = 3;
-	const int columns = 3;
-	
-	// outer loop iterates through each row
+	// outer loop iterates through rows
 	for (int i = 0; i < rows; i++)
 	{
-		// inner loop iterates through each element in the row
+		// inner loop iterates through elements in current row
 		for (int e = 0; e < columns; e++)
 		{
 			std::cout << positions[i][e];
 		}
 		std::cout << '\n';
+	}
+}
+
+void pseudoRefresh()
+{
+	for (int i = 0; i < 6; i++)
+	{
+		std::cout << i << '\n';
+	}
+}
+
+int main()
+{
+	while (true)
+	{
+		printPositions();
+
+		int row;
+		int column;
+
+		std::cout << "enter a row: ";
+		std::cin >> row;
+		std::cout << "enter a column: ";
+		std::cin >> column;
+		row -= 1;
+		column -= 1;
+
+		positions[row][column] = 1;
+
+		printPositions();
+		break;
 	}
 
 	return 0;
